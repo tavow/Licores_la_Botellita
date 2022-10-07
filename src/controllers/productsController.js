ae652c61e16e805b441742de6a36dc482566aa4d
tavow <gwehdeking@summa-sci.com>
2022-10-07 11:52:07 -0500

    Borrando imagenes de prueba

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -55,17 +55,6 @@ const controladorProductos = {
     // res.render('product-edit-form', { productToEdit })
   },
   vino: (req, res) => {
-    // let categoria = 'vino';
-    // let producto = [];
-    // for (let i = 0; i < products.length; i++) {
-    //   if (products[i].categoria == categoria) {
-    //     producto = producto.concat(products[i]);
-    //   }
-    // }
-    // console.log(producto);
-    // res.render("productos2", { producto: producto });
-    // res.send('Listado de vinos');
-
     var producto = Productos.findAll({
       where: {
         categoria: {
@@ -112,17 +101,6 @@ const controladorProductos = {
       });
   },
   whisky: (req, res) => {
-    // let categoria = 'whisky';
-    // let producto = [];
-    // for (let i = 0; i < products.length; i++) {
-    //   if (products[i].categoria == categoria) {
-    //     producto = producto.concat(products[i]);
-    //   }
-    // }
-    // console.log(producto);
-    // res.render("productos2", { producto: producto });
-    // res.send('Listado de whisky');
-
     var producto = Productos.findAll({
       where: {
         categoria: {
@@ -138,16 +116,6 @@ const controladorProductos = {
   },
   tequila: (req, res) => {
     let categoria = 'tequila';
-    // console.log('categoria');
-    // let producto = [];
-    // for (let i = 0; i < products.length; i++) {
-    //   if (products[i].categoria == categoria) {
-    //     producto = producto.concat(products[i]);
-    //   }
-    // }
-    // console.log(producto);
-    // res.render("productos2", { producto: producto });
-    // res.send('Listado de tequila');
     var producto = Productos.findAll({
       where: {
         categoria: {
@@ -162,15 +130,6 @@ const controladorProductos = {
   },
   coctel: (req, res) => {
     let categoria = 'coctel';
-    // let producto = [];
-    // for (let i = 0; i < products.length; i++) {
-    //   if (products[i].categoria == categoria) {
-    //     producto = producto.concat(products[i]);
-    //   }
-    // }
-    // console.log(producto);
-    // res.render("productos2", { producto: producto });
-    // res.send('Listado de coctel');
     var producto = Productos.findAll({
       where: {
         categoria: {
@@ -186,11 +145,6 @@ const controladorProductos = {
   detalle: (req, res) => {
     //res.send("Receta  " + req.params.idProducto);
     let id = req.params.idProducto;
-
-    // let producto = products.find((producto) => producto.id == id);
-    // let categoria = producto.categoria;
-    // console.log(categoria);
-
     Productos.findByPk(id)
       .then(function (producto) {
         console.log("Nos vamos al detalle del producto")
@@ -202,8 +156,6 @@ const controladorProductos = {
   // Delete - Delete one product from DB
   destroy: (req, res) => {
     console.log("Borrando un producto")
-    // let id = req.params.id;
-    // let finalProducts = products.filter(producto => producto.id != id);
     Productos.destroy({
       where: {
         idproducto: req.params.id
@@ -215,83 +167,83 @@ const controladorProductos = {
 
   store: (req, res) => {
     let img
-console.log(req.files);
-console.log("Creando el producto")
-if (req.files[0] != undefined) {
-  img = req.files[0].filename
-} else {
-  img = 'default-image.png'
-}
-console.log(img);
+    console.log(req.files);
+    console.log("Creando el producto")
+    if (req.files[0] != undefined) {
+      img = req.files[0].filename
+    } else {
+      img = 'default-image.png'
+    }
+    console.log(img);
 
-const resultValidation = validationResult(req);
-if (resultValidation.errors.length > 0) {
-  return res.render('product-create-form',
-    {
-      errors: resultValidation.mapped(),
-      oldData: req.body
-    });
-}
-
-if (req.files) {
-  console.log("Create");
-  Productos.create({
-    nombre: req.body.nombre,
-    descripcion: req.body.descripcion,
-    precio: req.body.precio,
-    descuento: req.body.descuento,
-    categoria: req.body.categoria,
-    tamano: req.body.tamano,
-    tipo: req.body.tipo,
-    img: img,
-    datatimeproducto: datatimeproducto
-  }).then((product) => res.redirect('/products'))
-  res.render('index')
-} else {
-  res.render('index')
-}
+    const resultValidation = validationResult(req);
+    if (resultValidation.errors.length > 0) {
+      return res.render('product-create-form',
+        {
+          errors: resultValidation.mapped(),
+          oldData: req.body
+        });
+    }
+    let datatimeproducto = "1999-06-06";
+    if (req.files) {
+      console.log("Create");
+      Productos.create({
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        descuento: req.body.descuento,
+        categoria: req.body.categoria,
+        tamano: req.body.tamano,
+        tipo: req.body.tipo,
+        img: img,
+        datatimeproducto: datatimeproducto
+      }).then((product) => res.redirect('/products'))
+      res.render('index')
+    } else {
+      res.render('index')
+    }
 
   },
-update: (req, res) => {
-  let id = req.params.id;
-  var products = Productos.findByPk(id)
-    .then(function (producto) {
-      console.log("Actualizando el producto")
-      // res.render("product-edit-form", { productToEdit: producto })
-    })
+  update: (req, res) => {
+    let id = req.params.id;
+    var products = Productos.findByPk(id)
+      .then(function (producto) {
+        console.log("Actualizando el producto")
+        // res.render("product-edit-form", { productToEdit: producto })
+      })
 
-  const resultValidation = validationResult(req);
+    const resultValidation = validationResult(req);
+    let datatimeproducto = "1999-06-06";
+    Promise.any([products])
+      .then(function (products) {
+        if (resultValidation.errors.length > 0) {
+          return res.render('EditProduct',
+            {
+              errors: resultValidation.mapped(),
+              oldData: req.body,
+              products: products
+            });
 
-  Promise.any([products])
-    .then(function (products) {
-      if (resultValidation.errors.length > 0) {
-        return res.render('EditProduct',
-          {
-            errors: resultValidation.mapped(),
-            oldData: req.body,
-            products: products
-          });
+        } else {
+          Productos.update({
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            descuento: req.body.descuento,
+            categoria: req.body.categoria,
+            tamano: req.body.tamano,
+            tipo: req.body.tipo,
+            img: req.body.img,
+            datatimeproducto: req.body.datatimeproducto
+          },
+            {
+              where: {
+                idproducto: req.params.id
+              }
+            }).then((product) => res.redirect('/products'))
+        }
+      })
 
-      } else {
-        Productos.update({
-          nombre: req.body.nombre,
-          descripcion: req.body.descripcion,
-          precio: req.body.precio,
-          descuento: req.body.descuento,
-          categoria: req.body.categoria,
-          tamano: req.body.tamano,
-          tipo: req.body.tipo,
-          img: req.body.img,
-          datatimeproducto: req.body.datatimeproducto
-        },
-          {
-            where: {
-              idproducto: req.params.id
-            }
-          }).then((product) => res.redirect('/products'))
-      }
-    })
-
-}
+  }
 };
 module.exports = controladorProductos;
